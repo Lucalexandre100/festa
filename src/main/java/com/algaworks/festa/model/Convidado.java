368bf98ce72da6cce493a793c13a06d8b2deb5b1
ap_lucas <l.alexandre100@gmail.com>
2018-09-13 23:15:15 -0300

    Inicial -

---
+++ src/main/java/com/algaworks/festa/model/Convidado.java
@@ -0,0 +1,38 @@
+package com.algaworks.festa.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Convidado {
+
+	public static final long serialVersionUID =1L;
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	private String nome;
+	private Integer quantidadeAcompanhantes;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getQuantidadeAcompanhantes() {
+		return quantidadeAcompanhantes;
+	}
+	public void setQuantidadeAcompanhantes(Integer quantidadeAcompanhantes) {
+		this.quantidadeAcompanhantes = quantidadeAcompanhantes;
+	}
+
+	
+}
